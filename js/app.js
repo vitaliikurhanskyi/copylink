485f498cfeb1afd249f441a118db48a347c5aa4e
vitaliikurhanskyi <vitalii.kurhanskyi@gmail.com>
2022-07-29 14:53:34 +0300

    copy

---
+++ js/app.js
@@ -0,0 +1,14 @@
+"use strict"
+
+let copyText = document.querySelector(".copy-text");
+
+copyText.querySelector("button").addEventListener("click", function () {
+	let input = copyText.querySelector("input.text");
+	input.select();
+	document.execCommand("copy");
+	copyText.classList.add("active");
+	window.getSelection().removeAllRanges();
+	setTimeout(function () {
+		copyText.classList.remove("active");
+	}, 1300);
+});
